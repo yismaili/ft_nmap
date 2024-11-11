e6fa73eb7549bb3f11606f93c200e8f5d62be7cd
younes ismaili <ismailiyounes20@gmail.com>
2024-11-11 18:21:41 +0100

    parse arguments

--- includes/scanner.h
+++ includes/scanner.h
@@ -1,17 +1,47 @@
 #ifndef SCANNER_H
 #define SCANNER_H
+
 #include <netinet/ip.h>
 #include <netinet/tcp.h>
 #include <arpa/inet.h>
 #include <sys/socket.h>
 #include <pcap/pcap.h>
+#include <string.h>  
+#include <stdlib.h>   
+#include <pthread.h>
+#include <unistd.h>
 
 #define CAPTURE_TIMEOUT 1000
+#define MAX_THREADS 250
+#define DEFAULT_TIMEOUT 2
 
+//define scan types
+typedef enum {
+    SCAN_SYN = 1,
+    SCAN_NULL = 2,
+    SCAN_ACK = 4,
+    SCAN_FIN = 8,
+    SCAN_XMAS = 16,
+    SCAN_UDP = 32
+} scan_type_t;
+
+//define configuration structure
+typedef struct {
+    char target_ip[16];
+    int start_port;
+    int end_port;
+    int thread_count;
+    unsigned int scan_types;
+    double timeout;
+    pthread_mutex_t mutex;
+} t_config;
+
+//define scanner context
 typedef struct {
     pcap_t *handle;
     int raw_socket;
     pthread_mutex_t *mutex;
+    t_config *config;
 } t_context;
 
 int initialize_scanner(t_context *ctx);
--- src/network/scanner.c
+++ src/network/scanner.c
@@ -1,17 +1,16 @@
 #include "../../includes/scanner.h"
-#include <unistd.h>
 
 int initialize_scanner(t_context *ctx) {
     char errbuf[PCAP_ERRBUF_SIZE];
 
-    // Create raw socket
+    // create raw socket
     ctx->raw_socket = socket(AF_INET, SOCK_RAW, IPPROTO_RAW);
     if (ctx->raw_socket < 0) {
         perror("Failed to create raw socket");
         return -1;
     }
 
-    // Set socket options 
+    // set socket options 
     int one = 1;
     if (setsockopt(ctx->raw_socket, IPPROTO_IP, IP_HDRINCL, &one, sizeof(one)) < 0) {
         perror("Failed to set IP_HDRINCL");
@@ -29,21 +28,3 @@ int initialize_scanner(t_context *ctx) {
 
     return 0;
 }
-
-
-int main() {
-    t_context ctx;
-
-    if (initialize_scanner(&ctx) == 0) {
-        printf("successfully.\n");
-
-        pcap_close(ctx.handle);
-        close(ctx.raw_socket);
-        printf("Resources cleaned up.\n");
-    } else {
-        fprintf(stderr, "Failed\n");
-    }
-
-    return 0;
-}
-
