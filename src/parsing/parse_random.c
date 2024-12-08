6a93d51474309e6bd9a56d3b8c3a2d30547524c1
Youssef Labtaimi <ylabtaim@e3r3p2.1337.ma>
2024-12-08 20:11:03 +0100

    fixed bug of random ips removing existing ips

--- src/parsing/parse_random.c
+++ src/parsing/parse_random.c
@@ -22,27 +22,28 @@ bool parse_random(char *random_str, t_scan_config *config) {
         return false;
     }
     
-    config->random_hosts = random_val;
-    
-    config->target_ips = malloc(random_val * sizeof(char*));
-    if (!config->target_ips) {
+    int current_count = config->ip_count;
+    char **new_target_ips = realloc(config->target_ips, 
+                                   (current_count + random_val) * sizeof(char*));
+    if (!new_target_ips) {
         fprintf(stderr, "Memory allocation failed\n");
         return false;
     }
+    config->target_ips = new_target_ips;
     
     srand(time(NULL));
     for (int i = 0; i < random_val; i++) {
-        config->target_ips[i] = generate_random_ip();
-        if (!config->target_ips[i]) {
-            for (int j = 0; j < i; j++) {
+        config->target_ips[current_count + i] = generate_random_ip();
+        if (!config->target_ips[current_count + i]) {
+            for (int j = current_count; j < current_count + i; j++) {
                 free(config->target_ips[j]);
             }
-            free(config->target_ips);
+
             fprintf(stderr, "Failed to generate random IP\n");
             return false;
         }
     }
     
-    config->ip_count = random_val;
+    config->ip_count = current_count + random_val;
     return true;
 } 
